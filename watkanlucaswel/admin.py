37605793c4bfb27faf7bb3547af3315d9f7f03e5
Teun <teun.willems+github@live.nl>
2018-09-16 19:07:48 +0200

    Uploaded version

--- watkanlucaswel/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from watkanlucaswel.models import WKLWImage
-
-admin.site.register(WKLWImage)
--- watkanlucaswel/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('randomimage', views.randomimage, name='randomimage')
-]
